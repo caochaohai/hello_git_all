521175d83bf4110539dc93c50103e42d655b7cf1
caochaohai <947220846@qq.com>
2019-05-05 16:54:11 +0800

    提交test_login

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+class TestLogin:
+
+    def test_1(self):
+        print("test1")
+
+    def test_11(self):
+        print("test11")
+
+    def test_2(self):
+        print("test2")
+
+    def test_14(self):
+        print("test14")
